023d08fdd5615df4d178e62e3d6558d765e71b96
Michael Scherbela <michael.scherbela@gmail.com>
2023-08-02 12:24:57 +0200

    Commit before inspecting Fn denominator

--- oscillating_screw/save_animation.py
+++ oscillating_screw/save_animation.py
@@ -6,12 +6,18 @@ from matplotlib.animation import FuncAnimation
 
 # alpha_in_deg = 21
 params = get_screw_params(
-    length=6, diameter=0.8, head_diameter=4, head_thickness=0.2, mu=0.25, mu_roll=0.01, alpha=0.21
+    length=6, 
+    diameter=0.8,
+    head_diameter=4,
+    head_thickness=0.2,
+    mu=0.2,
+    mu_roll=0.01,
+    alpha=np.radians(18)
 )
 
 
 dt = 1e-3
-t_end = 5.0
+t_end = 3.0
 phi_init_deg = 135
 
 t_values, R_values, phi_values, theta_values, R_dot_values, phi_dot_values, theta_dot_values = simulate(
@@ -21,27 +27,37 @@ n_t = len(t_values)
 x1 = R_values + np.stack([np.sin(phi_values), -np.cos(phi_values)], axis=1) * (params.s1 - params.sc)
 x2 = R_values + np.stack([np.sin(phi_values), -np.cos(phi_values)], axis=1) * (params.s2 - params.sc)
 
-
 plt.close("all")
-fig, ax = plt.subplots(figsize=(17, 5))
+fig_t, (ax_phi, ax_y) = plt.subplots(1, 2, figsize=(9, 6))
+ax_phi.plot(t_values, np.degrees(phi_values))
+ax_phi.set_xlabel("t / s")
+ax_phi.set_ylabel("$\\varphi$ / deg", labelpad=-5)
+ax_phi.set_ylim([-360, 360])
 
-slowdown = 5
-fps = 25
-n_skip = int(1 / (dt * slowdown * fps))
-fontsize = 16
+ax_y.plot(t_values, x1[:, 1], label="x1")
+ax_y.plot(t_values, x2[:, 1], label="x2")
+ax_y.set_xlabel("t / s")
+ax_y.set_ylabel("y / cm", labelpad=-5)
 
-ax.plot(x1[0::n_skip, 1], x1[0::n_skip, 0], color="C0", alpha=0.3)
-ax.plot(x2[0::n_skip, 1], x2[0::n_skip, 0], color="C1", alpha=0.3)
-(marker1,) = plt.plot([], [], marker="o", markersize=10, color="C0")
-(marker2,) = plt.plot([], [], marker="o", markersize=20, color="C1")
-(connection,) = plt.plot([], [], color="k")
-text = ax.text(0.02, 0.9, "", transform=ax.transAxes, fontsize=fontsize)
+# fig, ax = plt.subplots(figsize=(17, 5))
 
-# ax.axis("equal")
-ax.invert_xaxis()
-ax.set_xlabel("Position along the ramp / cm", fontsize=fontsize)
-ax.set_ylabel("Position across the ramp / cm", fontsize=fontsize)
-ax.tick_params(labelsize=fontsize)
+# slowdown = 5
+# fps = 25
+# n_skip = int(1 / (dt * slowdown * fps))
+# fontsize = 16
+
+# ax.plot(x1[0::n_skip, 1], x1[0::n_skip, 0], color="C0", alpha=0.3)
+# ax.plot(x2[0::n_skip, 1], x2[0::n_skip, 0], color="C1", alpha=0.3)
+# (marker1,) = plt.plot([], [], marker="o", markersize=10, color="C0")
+# (marker2,) = plt.plot([], [], marker="o", markersize=20, color="C1")
+# (connection,) = plt.plot([], [], color="k")
+# text = ax.text(0.02, 0.9, "", transform=ax.transAxes, fontsize=fontsize)
+
+# # ax.axis("equal")
+# ax.invert_xaxis()
+# ax.set_xlabel("Position along the ramp / cm", fontsize=fontsize)
+# ax.set_ylabel("Position across the ramp / cm", fontsize=fontsize)
+# ax.tick_params(labelsize=fontsize)
 
 #%%
 def run_animation(i):
